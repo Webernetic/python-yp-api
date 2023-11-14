a01944d1db99dd791107e9a4bca8e8e186e24c59
den4ikos <denasius89@yandex.ru>
2023-11-14 22:11:00 +0300

    fixed docker compose and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM python:3.9-alpine
-MAINTAINER Webernetic YP App Ltd
 
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
-RUN pip install -r /requirements.txt
+
+RUN apk update \
+    && apk add postgresql-dev gcc python3-dev musl-dev
+
+RUN pip install --no-cache-dir -r /requirements.txt
 
 RUN mkdir /app
 WORKDIR /app
--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ksyubarber',
+        'USER': 'ksyubarber',
+        'PASSWORD': 'ksyubarber',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
 }
 
@@ -121,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS=True
